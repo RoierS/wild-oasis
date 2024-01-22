9c3856721c326a8a483d4fb6dec881aa64f46c7a
Roman Iermoliuk <iermoliuk.roman@gmail.com>
2024-01-22 10:46:53 +0100

    ci: :wrench: configure project

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,166 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2020: true, jest: true, node: true },
   extends: [
     'eslint:recommended',
+    'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:import/typescript',
+    'plugin:prettier/recommended',
+    'prettier',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: [
+    'dist',
+    '.eslintrc.cjs',
+    'vite.config.ts',
+    'postcss.config.js',
+  ],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.node.json'],
+    tsconfigRootDir: __dirname,
+  },
+  plugins: [
+    'react-refresh',
+    'react',
+    'react-hooks',
+    'prettier',
+    'import',
+    '@typescript-eslint',
+  ],
   rules: {
+    semi: ['error', 'always'],
+    'no-multiple-empty-lines': [
+      'error',
+      {
+        max: 1,
+        maxEOF: 1,
+        maxBOF: 1,
+      },
+    ],
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'import/no-extraneous-dependencies': 'off',
+    'react/function-component-definition': [
+      'error',
+      {
+        namedComponents: 'arrow-function',
+        unnamedComponents: 'arrow-function',
+      },
+    ],
+    'react/react-in-jsx-scope': ['off'],
+    'react/jsx-uses-react': ['off'],
+    'react/jsx-props-no-spreading': ['off'],
+    'react/no-unescaped-entities': ['off'],
+    'react/prop-types': 'off',
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        selector: 'variable',
+        format: ['camelCase', 'PascalCase', 'UPPER_CASE'],
+        leadingUnderscore: 'allow',
+      },
+      {
+        selector: 'variable',
+        types: ['boolean'],
+        format: ['PascalCase'],
+        prefix: ['is', 'can', 'has', 'should', 'will', 'did'],
+      },
+    ],
+    'import/order': [
+      'error',
+      {
+        groups: [
+          'external',
+          'internal',
+          'builtin',
+          'parent',
+          'sibling',
+          'index',
+          'object',
+          'type',
+          'unknown',
+        ],
+        pathGroups: [
+          {
+            pattern: 'react',
+            group: 'external',
+            position: 'before',
+          },
+          {
+            pattern: 'react-hook-form',
+            group: 'external',
+            position: 'before',
+          },
+          {
+            pattern: 'axios',
+            group: 'external',
+            position: 'before',
+          },
+          {
+            pattern: '@hookform/**',
+            group: 'external',
+            position: 'after',
+          },
+          {
+            pattern: '@/*',
+            group: 'internal',
+            position: 'after',
+          },
+          {
+            pattern: 'components/**',
+            group: 'internal',
+            position: 'after',
+          },
+          {
+            pattern: 'assets/**',
+            group: 'internal',
+            position: 'after',
+          },
+          {
+            pattern: '*.{css,scss}',
+            group: 'unknown',
+            position: 'after',
+          },
+          {
+            pattern: './**.{css,scss}',
+            group: 'unknown',
+            position: 'after',
+          },
+        ],
+        pathGroupsExcludedImportTypes: [],
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+        warnOnUnassignedImports: true,
+        'newlines-between': 'always-and-inside-groups',
+      },
+    ],
+    'import/no-unresolved': 'error',
+    'import/first': 'error',
+    'import/no-duplicates': 'error',
+    quotes: ['error', 'single'],
+    'no-console': 'error',
+    'no-debugger': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    '@typescript-eslint/no-explicit-any': 'error',
   },
-}
+  settings: {
+    react: {
+      pragma: 'React',
+      version: 'detect',
+    },
+    'import/resolver': {
+      typescript: {},
+    },
+  },
+};
