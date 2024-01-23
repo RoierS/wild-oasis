fc7b19f9a2e2b218a619bd0c3c9d7096c28b3b0a
Roman Iermoliuk <iermoliuk.roman@gmail.com>
2024-01-23 08:50:14 +0100

    feat: build App layout

--- src/ui/AppLayout.tsx
+++ src/ui/AppLayout.tsx
@@ -1,18 +1,32 @@
 import { Outlet } from 'react-router-dom';
 
+import styled from 'styled-components';
+
 import Header from './Header';
 import Sidebar from './Sidebar';
 
+const StyledAppLayout = styled.div`
+  display: grid;
+  grid-template-columns: 26rem 1fr;
+  grid-template-rows: auto 1fr;
+  height: 100dvh;
+`;
+
+const Main = styled.main`
+  background-color: var(--color-grey-50);
+  padding: 4rem 4.8rem 6.4rem;
+`;
+
 const AppLayout: React.FC = () => {
   return (
-    <div>
+    <StyledAppLayout>
       <Header />
       <Sidebar />
 
-      <main>
+      <Main>
         <Outlet />
-      </main>
-    </div>
+      </Main>
+    </StyledAppLayout>
   );
 };
 
