83e71fe8879145111a50a67c9a7b7c90f5aa1b37
Roman Iermoliuk <iermoliuk.roman@gmail.com>
2024-01-25 11:09:08 +0100

    refactor: move FormRow component to separate file

--- src/features/cabins/CreateCabinForm.tsx
+++ src/features/cabins/CreateCabinForm.tsx
@@ -2,51 +2,15 @@ import { useForm } from 'react-hook-form';
 
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import toast from 'react-hot-toast';
-import styled from 'styled-components';
 
 import { createCabin } from '@/services/apiCabins';
 import { ICabin } from '@/types/cabin';
 import Button from '@/ui/Button/Button';
 import FileInput from '@/ui/FileInput/FileInput';
 import Form from '@/ui/Form/Form';
+import FormRow from '@/ui/FormRow/FormRow';
 import Input from '@/ui/Input/Input';
 import Textarea from '@/ui/Textarea/Textarea';
-
-const FormRow = styled.div`
-  display: grid;
-  align-items: center;
-  grid-template-columns: 24rem 1fr 1.2fr;
-  gap: 2.4rem;
-
-  padding: 1.2rem 0;
-
-  &:first-child {
-    padding-top: 0;
-  }
-
-  &:last-child {
-    padding-bottom: 0;
-  }
-
-  &:not(:last-child) {
-    border-bottom: 1px solid var(--color-grey-100);
-  }
-
-  &:has(button) {
-    display: flex;
-    justify-content: flex-end;
-    gap: 1.2rem;
-  }
-`;
-
-const Label = styled.label`
-  font-weight: 500;
-`;
-
-// const Error = styled.span`
-//   font-size: 1.4rem;
-//   color: var(--color-red-700);
-// `;
 
 const CreateCabinForm = () => {
   const queryClient = useQueryClient();
@@ -65,7 +29,14 @@ const CreateCabinForm = () => {
     onError: (err) => toast.error(err.message),
   });
 
-  const { register, handleSubmit, reset } = useForm<ICabin>();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm<ICabin>({
+    mode: 'onChange',
+  });
 
   const onSubmit = (data: ICabin) => {
     mutate(data);
@@ -73,44 +44,76 @@ const CreateCabinForm = () => {
 
   return (
     <Form onSubmit={handleSubmit(onSubmit)}>
-      <FormRow>
-        <Label htmlFor="name">Cabin name</Label>
-        <Input type="text" id="name" {...register('name')} />
+      <FormRow labelName="Cabin name" errorMessage={errors?.name?.message}>
+        <Input
+          type="text"
+          id="name"
+          disabled={isCreating}
+          {...register('name', {
+            required: 'This field is required',
+          })}
+        />
       </FormRow>
 
-      <FormRow>
-        <Label htmlFor="maxCapacity">Maximum capacity</Label>
-        <Input type="number" id="maxCapacity" {...register('maxCapacity')} />
+      <FormRow
+        labelName="Maximum capacity"
+        errorMessage={errors?.maxCapacity?.message}
+      >
+        <Input
+          type="number"
+          id="maxCapacity"
+          disabled={isCreating}
+          {...register('maxCapacity', {
+            required: 'This field is required',
+            max: {
+              value: 10,
+              message: 'Capacity cannot be greater than 10',
+            },
+          })}
+        />
       </FormRow>
 
-      <FormRow>
-        <Label htmlFor="regularPrice">Regular price</Label>
-        <Input type="number" id="regularPrice" {...register('regularPrice')} />
+      <FormRow
+        labelName="Regular price"
+        errorMessage={errors?.regularPrice?.message}
+      >
+        <Input
+          type="number"
+          id="regularPrice"
+          disabled={isCreating}
+          {...register('regularPrice', {
+            required: 'This field is required',
+          })}
+        />
       </FormRow>
 
-      <FormRow>
-        <Label htmlFor="discount">Discount</Label>
+      <FormRow labelName="Discount" errorMessage={errors?.discount?.message}>
         <Input
           type="number"
           id="discount"
-          defaultValue={0}
-          {...register('discount')}
+          disabled={isCreating}
+          {...register('discount', {
+            required: 'This field is required',
+          })}
         />
       </FormRow>
 
-      <FormRow>
-        <Label htmlFor="description">Description for website</Label>
+      <FormRow
+        labelName="Description for website"
+        errorMessage={errors?.description?.message}
+      >
         <Textarea
           type="number"
           id="description"
-          defaultValue=""
-          {...register('description')}
+          disabled={isCreating}
+          {...register('description', {
+            required: 'This field is required',
+          })}
         />
       </FormRow>
 
-      <FormRow>
-        <Label htmlFor="image">Cabin photo</Label>
-        <FileInput id="image" accept="image/*" />
+      <FormRow labelName="Cabin photo" errorMessage={errors?.image?.message}>
+        <FileInput id="image" accept="image/*" disabled={isCreating} />
       </FormRow>
 
       <FormRow>
@@ -126,6 +129,22 @@ const CreateCabinForm = () => {
           Add cabin
         </Button>
       </FormRow>
+
+      {/* 
+
+      <FormRow>
+        <Button
+          $variation="secondary"
+          $size="medium"
+          type="reset"
+          onClick={() => reset()}
+        >
+          Cancel
+        </Button>
+        <Button $variation="secondary" $size="medium" disabled={isCreating}>
+          Add cabin
+        </Button>
+      </FormRow> */}
     </Form>
   );
 };
---
+++ src/ui/FormRow/FormRow.tsx
@@ -0,0 +1,63 @@
+import { ReactNode, isValidElement } from 'react';
+
+import { styled } from 'styled-components';
+
+const StyledFormRow = styled.div`
+  display: grid;
+  align-items: center;
+  grid-template-columns: 24rem 1fr 1.2fr;
+  gap: 2.4rem;
+
+  padding: 1.2rem 0;
+
+  &:first-child {
+    padding-top: 0;
+  }
+
+  &:last-child {
+    padding-bottom: 0;
+  }
+
+  &:not(:last-child) {
+    border-bottom: 1px solid var(--color-grey-100);
+  }
+
+  &:has(button) {
+    display: flex;
+    justify-content: flex-end;
+    gap: 1.2rem;
+  }
+`;
+
+const Label = styled.label`
+  font-weight: 500;
+`;
+
+const Error = styled.span`
+  font-size: 1.4rem;
+  color: var(--color-red-700);
+`;
+
+interface IFormRowProps {
+  labelName?: string;
+  errorMessage?: string;
+  children: ReactNode;
+}
+
+const FormRow: React.FC<IFormRowProps> = ({
+  labelName,
+  errorMessage,
+  children,
+}) => {
+  return (
+    <StyledFormRow>
+      {labelName && isValidElement(children) && (
+        <Label htmlFor={children.props.id}>{labelName}</Label>
+      )}
+      {children}
+      {errorMessage && <Error>{errorMessage}</Error>}
+    </StyledFormRow>
+  );
+};
+
+export default FormRow;
