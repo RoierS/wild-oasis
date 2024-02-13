e33c68f94db52fa73702b92031112cab0ac353b5
Roman Iermoliuk <iermoliuk.roman@gmail.com>
2024-02-13 09:38:26 +0100

    feat: implement api-side bookings filtering by status

--- src/hooks/useBooking.ts
+++ src/hooks/useBooking.ts
@@ -1,15 +1,27 @@
 import { useQuery } from '@tanstack/react-query';
 
-import { getBookings } from '@/services/apiBookings';
+import { useSearchParams } from 'react-router-dom';
+
+import { IFilter, getBookings } from '@/services/apiBookings';
 
 export const useBookings = () => {
+  const [searchParams] = useSearchParams();
+
+  // Filter
+  const filterValue = searchParams.get('status');
+
+  const filter: IFilter | null =
+    !filterValue || filterValue === 'all'
+      ? null
+      : { field: 'status', value: filterValue };
+
   const {
     isLoading,
     data: bookings,
     error,
   } = useQuery({
-    queryKey: ['bookings'],
-    queryFn: getBookings,
+    queryKey: ['bookings', filter],
+    queryFn: () => getBookings({ filter }),
   });
 
   return { isLoading, bookings, error };
--- src/services/apiBookings.ts
+++ src/services/apiBookings.ts
@@ -1,11 +1,33 @@
 import supabase from './supabase';
 
-export const getBookings = async () => {
-  const { data, error } = await supabase
+export interface IFilter {
+  field: string;
+  value: number | string;
+  method?: 'gte';
+}
+
+interface getBookingsProps {
+  filter: IFilter | null;
+  sortBy?: string;
+}
+
+export const getBookings = async ({ filter, sortBy }: getBookingsProps) => {
+  let query = supabase
     .from('bookings')
     .select(
       'id, created_at, numNights, startDate, endDate, status, totalPrice, cabins(name), guests(fullName, email)',
     );
+
+  // Filter
+  if (filter) {
+    const { method, field, value } = filter;
+    query = method ? query.gte(field, value) : query.eq(field, value);
+  }
+
+  // eslint-disable-next-line no-console
+  console.log(sortBy);
+
+  const { data, error } = await query;
 
   if (error) {
     console.error(error);
